5123f04fbe0503c4ba805299d171189b0092ad14
Ian Angelo Aragoza <khelzor31415@gmail.com>
2021-03-28 13:52:58 +0800

    added data and graphing

--- 03 - Bifilar Pendulum Part 1/graphs.py
+++ 03 - Bifilar Pendulum Part 1/graphs.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
+import seaborn as sns
 import math
 
 g = 9.81 # meter per second per second
+df_lateral = pd.read_csv("lateral_data.csv")
+df_swing = pd.read_csv("swing_data.csv")
+df_torsional = pd.read_csv("torsional_data.csv")
 
 # R is the radius of the rod
 # L is the length of the rod
@@ -18,32 +21,39 @@ def theory_lateral(R,H):
 def theory_torsional(R,H,L,d):
     return 2*math.pi*(L/d)/math.sqrt(12)*math.sqrt((H-R)/g)
 
-# Insert lenghts used
-# L = {{}}
 
-R = 1
+R = 0.005
 M = 1
-L = 1
-d = 1
+L = 0.215
+d = 0.071 #confirm
 
 vec_swing = np.vectorize(theory_swing)
 vec_lateral = np.vectorize(theory_lateral)
 vec_torsional = np.vectorize(theory_torsional)
 
-H = np.linspace(10,30,200)
+H = np.linspace(0.1,0.6,200)
 
 T_swing = vec_swing(R, H)
 T_lateral = vec_lateral(R, H)
 T_torsional = vec_torsional(R,H,L,d)
 
 plt.plot(H, T_swing)
+plt.scatter(df_swing.string_length, df_swing.period_ave)
+plt.xlabel("String Length (m)")
+plt.ylabel("Period (s)")
 plt.title('Swingin')
 plt.show()
 
 plt.plot(H, T_lateral)
+plt.scatter(df_lateral.string_length, df_lateral.period_ave)
+plt.xlabel("String Length (m)")
+plt.ylabel("Period (s)")
 plt.title('Lateral')
 plt.show()
 
 plt.plot(H, T_torsional)
+plt.scatter(df_torsional.string_length, df_torsional.period_ave)
+plt.xlabel("String Length (m)")
+plt.ylabel("Period (s)")
 plt.title('Torsional')
 plt.show()
